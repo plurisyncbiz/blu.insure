502d0695fa28bb07f62db8554cabd869706fce6a
Plurisync <pierre@plurisync.biz>
2025-12-09 13:29:53 +0200

    review.php

--- review.php
+++ review.php
@@ -371,18 +371,17 @@ function getPolicyDetails($id, $api_base){
 </div>
 
 <footer class="my-5 pt-5 text-muted text-center text-small">
-    <p style="font-size: 0.75em;">
-        <?php if(isset($data)) print_r($data); ?>
-    </p>
+    <?php if($_ENV['APP_ENV'] !== 'production'): ?>
+        <p style="font-size: 0.75em; color: red;">DEBUG: <?php print_r($serial_data); ?></p>
+    <?php endif; ?>
 
-    <p class="mb-1">&copy; 2025 Blue Label Data Solutions (Pty) Ltd</p>
+    <p class="mb-1">&copy; <?php echo date('Y'); ?> Blue Label Data Solutions (Pty) Ltd</p>
     <ul class="list-inline">
         <li class="list-inline-item"><a href="#">Privacy</a></li>
         <li class="list-inline-item"><a href="#">Terms</a></li>
         <li class="list-inline-item"><a href="#">Support</a></li>
     </ul>
 </footer>
-</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
