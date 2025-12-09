8a5a90eb402717b2bc1f38e82fd345b0edaf47cc
Plurisync <pierre@plurisync.biz>
2025-12-09 14:43:19 +0200

    confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -186,17 +186,17 @@ require_once '_bootstrap.php';
                             <p class="fw-bold pt-5">Don't have your previous policy documents?</p>
                             <p class="pt-3">That's OK, you can still apply for prepaid funeral cover. Send us your documents within 31 days from today, or our standard waiting period will apply to your policy.</p>
                             <p class="pt-3 pb-3">Send proof of cancellation to digitalfuneralsupport@sanlam.co.za</p>
-                            <div class="form-check mt-3 mb-5">
-                                <input class="form-check-input" type="checkbox" value="1" id="termsCheckbox" required>
-                                <label class="form-check-label" for="termsCheckbox">
-                                    I have read and agree to the
-                                    <a href="#" class="text-decoration-underline" data-bs-toggle="modal" data-bs-target="#termsModal" onclick="event.stopPropagation()">
-                                        Terms and Conditions
-                                    </a> about the product
-                                </label>
-                                <div class="invalid-feedback">
-                                    You must accept the terms to proceed.
-                                </div>
+                        </div>
+                        <div class="form-check mt-3 mb-5">
+                            <input class="form-check-input" type="checkbox" value="1" id="termsCheckbox" required>
+                            <label class="form-check-label" for="termsCheckbox">
+                                I have read and agree to the
+                                <a href="#" class="text-decoration-underline" data-bs-toggle="modal" data-bs-target="#termsModal" onclick="event.stopPropagation()">
+                                    Terms and Conditions
+                                </a> about the product
+                            </label>
+                            <div class="invalid-feedback">
+                                You must accept the terms to proceed.
                             </div>
                         </div>
 
